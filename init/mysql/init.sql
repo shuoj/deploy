be0ea5204ee3174ef309c7cec50ea41ee6a38c5b
Siyuan Zhang | 张豆 <kastnerorz@gmail.com>
2019-10-30 15:52:55 +0800

    Update init.sql

--- init/mysql/init.sql
+++ init/mysql/init.sql
@@ -7,5 +7,17 @@ create table if not exists authority
     primary key,
   name varchar(50) not null
 );
-insert into authority (id, name) values (1, 'ROLE_USER');
-insert into authority (id, name) values (2, 'ROLE_ADMIN');
+INSERT INTO authority (id, name) VALUES (1, 'ROLE_USER');
+INSERT INTO authority (id, name) VALUES (2, 'ROLE_ADMIN');
+INSERT INTO authority (id, name) VALUES (3, 'ROLE_TEACHER');
+
+create table idx_sequence
+(
+  name     varchar(20) not null
+    primary key,
+  next_idx bigint      null
+) engine = MyISAM;
+
+INSERT INTO idx_sequence (name, next_idx) VALUES ('problem', 1);
+INSERT INTO idx_sequence (name, next_idx) VALUES ('contest', 1);
+INSERT INTO idx_sequence (name, next_idx) VALUES ('group', 1);
